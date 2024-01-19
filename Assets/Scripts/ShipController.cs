ffae2964d3c44de269c58a45786c95608fa964cc
Gio <gohan1703@gmail.com>
2024-01-19 12:51:55 -0500

    CXFJDJGDJ

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+    public Rigidbody cube;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cube = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            cube.AddForce(Vector3.up * 20f);
+        }
     }
 }
